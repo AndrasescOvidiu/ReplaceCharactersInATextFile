90cf6393c9570702340bf636af4893faf45c0d6a
AndrasescOvidiu <andrasesc.ovidiu@gmail.com>
2018-07-08 20:03:47 +0300

    First commit

---
+++ ReplaceChars/ReplaceChars/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ReplaceChars
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.a1 = new System.Windows.Forms.TextBox();
+            this.b1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Replace and Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // a1
+            // 
+            this.a1.Location = new System.Drawing.Point(55, 73);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(100, 20);
+            this.a1.TabIndex = 1;
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(244, 72);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(100, 20);
+            this.b1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Replace this:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(242, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "With this:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 285);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox a1;
+        private System.Windows.Forms.TextBox b1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ReplaceChars/ReplaceChars/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReplaceChars
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public List<string> generateACharList()
+        {
+            List<string> chars = new List<string>();
+            if (a1.Text.Length > 0)
+            {
+                chars.Add(a1.Text);
+            }
+            return chars;
+        }
+        public List<string> generateBCharList()
+        {
+            List<string> chars = new List<string>();
+            if (b1.Text.Length > 0)
+            {
+                chars.Add(b1.Text);
+            }
+            return chars;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "C:\\Users\\Ovidiu\\Desktop";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            List<string> charsA = generateACharList();
+            List<string> charsB = generateBCharList();
+
+            MessageBox.Show(charsA[0]+ " " + charsB[0] );
+
+            String generatedFile = "";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+
+
+                    String path = "C:\\Users\\Ovidiu\\Desktop\\result.docs";
+
+
+                    if ((openFileDialog1.OpenFile()) != null)
+                    {
+                        var filestream = new System.IO.FileStream(openFileDialog1.FileName,
+                                          System.IO.FileMode.Open,
+                                          System.IO.FileAccess.Read,
+                                          System.IO.FileShare.ReadWrite);
+                        var file = new System.IO.StreamReader(filestream, System.Text.Encoding.UTF8, true, 128);
+                        String lineOfText = "";
+                        while ((lineOfText = file.ReadLine()) != null)
+                        {
+                            generatedFile += lineOfText+ Environment.NewLine;
+
+                            generatedFile =  generatedFile.Replace(Convert.ToChar(charsA[0]), Convert.ToChar(charsB[0]));
+                        }
+         
+                        File.WriteAllText(path, generatedFile);
+                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+            MessageBox.Show("Done");
+        }
+    }
+}
